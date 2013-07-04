998287b49935bc8d860505c5652233958afdd8aa
bhanu <bhanu@Z580.(none)>
2013-07-04 17:06:22 +0530

    bhanu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bhanu::Application.config.secret_token = '462869ce37163ea9cb619ba536e9aea400b8daedcb89653060c9bac88857f90f8db836ed51ed8169a256bab36357aa23e528c1ffaf185132018f94e1070958f9'
